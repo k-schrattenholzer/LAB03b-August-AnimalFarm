4495ba3d297413b37c6587018a3361672d58e88f
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-11 17:24:38 -0700

    added assetts, header element, and sounds to play on click

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // import functions and grab DOM elements
+const headerEl = document.getElementById('animal-click');
+//
+const horseEl = document.getElementById('horse-img');
+const dogEl = document.getElementById('dog-img');
+const catEl = document.getElementById('cat-img');
+//
+const horseSound = document.getElementById('horse-sound');
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
 
-// initialize state
+//
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+catEl.addEventListener('click', () => {
+  catSound.play ();
+  headerEl.textContent = "Cat";
+});
+dogEl.addEventListener('click', () => {
+  dogSound.play ();
+  headerEl.textContent = "Dog";
+});
+horseEl.addEventListener('click', () => {
+  horseSound.play ();
+  headerEl.textContent = "Horse";
+});
+
