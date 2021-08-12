f3dcf2a8a0adfc14eccf22fce1bc9b851efd76d9
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-12 10:58:34 -0700

    refactored code and fixed the timeout for the headerEl

--- app.js
+++ app.js
@@ -9,33 +9,42 @@ const horseSound = document.getElementById('horse-sound');
 const dogSound = document.getElementById('dog-sound');
 const catSound = document.getElementById('cat-sound');
 
+//Re-factored Code
+function playSoundChangeText(animalSound, animalName) {
+    animalSound.play();
+    headerEl.textContent = animalName;
+    setTimeout(() => {
+        headerEl.textContent = 'Pick another!';
+    }, 3000);
+}
+
 //
 
 catEl.addEventListener('click', () => {
-    catSound.play ();
-    headerEl.textContent = "Cat";
+    playSoundChangeText(catSound, 'little kitten');
 });
 dogEl.addEventListener('click', () => {
-    dogSound.play ();
-    headerEl.textContent = "Dog";
+    playSoundChangeText(dogSound, 'sweet pupper');
 });
 horseEl.addEventListener('click', () => {
-    horseSound.play ();
-    headerEl.textContent = "Horse";
+    playSoundChangeText(horseSound, 'majestic horse');
 });
 
 //listen for when the user hits a key, when they do examine this event thing. event is a callback here
 document.addEventListener('keydown', (event) => {
     if (event.key === 'c') {
         alert('you pressed C - C is for CAT');
-        catSound.play ();
+        playSoundChangeText(catSound, 'little kitten');
     }
     if (event.key === 'd') {
         alert('you pressed D - D is for DOG');
-        dogSound.play ();
+        playSoundChangeText(dogSound, 'sweet pupper');
     }
     if (event.key === 'h') {
         alert('you pressed H - H is for HORSE');
-        horseSound.play ();
+        playSoundChangeText(horseSound, 'majestic horse');
     }
 });
+
+
+
